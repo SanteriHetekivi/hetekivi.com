a719cf6ad696d1ce9505a9930c161850fcedbb2a
Santeri Hetekivi <santeri@hetekivi.com>
2019-06-29 18:35:34 +0300

    Skeleton UI for adding and removing items.

---
+++ web/wishlist/forms.py
@@ -0,0 +1,8 @@
+from .models import Item
+from django.forms import ModelForm
+
+
+class ItemForm(ModelForm):
+    class Meta:
+        model = Item
+        exclude = []
--- web/wishlist/views.py
+++ web/wishlist/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Item
+from .forms import ItemForm
+from django.shortcuts import redirect
 
 
-def index(request):
-    return HttpResponse("Hello, world.")
+def index(request, id=None):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = ItemForm()
+    else:
+        form = ItemForm()
+    wishlist = Item.objects.order_by('position')
+    template = loader.get_template('wishlist/index.html')
+    context = {
+        'wishlist': wishlist,
+        'form': form,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def delete(request, id):
+    Item.objects.filter(id=id).delete()
+    return redirect('/wishlist')
